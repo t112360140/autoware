554f68110badb5c921246e8d469c5d1287f0c0a9
t112360140 <t112360140@ntut.org.tw>
2025-12-04 14:29:21 +0800

    SPIE INIT

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+vcs import src < autoware.repos
+
+vcs pull src
+
+rosdep update
+
+rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
+
+
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
